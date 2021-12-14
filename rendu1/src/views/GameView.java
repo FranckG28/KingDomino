c6eb4f55df9b8de4b96da5fc80f848f0a3311042
GUTMANN FRANCK <fgutmann@etu.unistra.fr>
2021-12-14 15:11:48 +0100

    WIP on views

--- rendu1/src/views/GameView.java
+++ rendu1/src/views/GameView.java
@@ -4,19 +4,44 @@ import controllers.GameController;
 import models.*;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class GameView extends JFrame implements GameObserver {
 
     private GameController controller;
 
-    public GameView(GameController controller) {
+    private JPanel drawPanel = new JPanel();
+
+    private JLabel gameLabel = new JLabel("Instruction");
+    private JLabel playerLabel = new JLabel("Joueur");
+
+    public GameView(GameController controller, Game game) {
 
         this.controller = controller;
 
+        // CONTENEUR PRINCIPAL :
 
-        // PARAMETRE ET OUVERTURE
+        getContentPane().setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
 
-        setSize(800, 800);
+        // CREATION DES KINGDOMVIEWS QUI OBSERVENT LEURS ROYAUMES
+
+        JPanel kingdomGrid = new JPanel(new GridLayout(2,2));
+        for (Player p:game.getPlayers()) {
+            Kingdom kingdom = p.getKingdom();
+            KingdomView kview = new KingdomView(kingdom);
+            kingdom.addObserver(kview);
+            kingdomGrid.add(kview);
+        }
+
+        // PLACEMENTS DES LABELS
+
+
+
+
+        // PARAMETRES ET OUVERTURE
+
+        setSize(1200, 800);
 
         setTitle("KingDomino - Jeu");
         setVisible(true);
@@ -24,6 +49,9 @@ public class GameView extends JFrame implements GameObserver {
     }
 
     public void reactGame(Game game) {
+        // REFRESH CURRENT PLAYER + INSTRUCTIONS
+
+        // REFRESH DRAW
 
     }
 
---
+++ rendu1/src/views/KingdomView.java
@@ -0,0 +1,66 @@
+package views;
+
+import models.Kingdom;
+import models.Tile;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class KingdomView extends JPanel implements KingdomObserver {
+
+    public KingdomView(Kingdom kingdom) {
+
+        final int borderWidth = 1;
+        final int rows = 5;
+        final int cols = 5;
+        setLayout(new GridLayout(rows, cols));
+        setBorder(BorderFactory.createEmptyBorder(2,2,2,2));
+
+        Tile[][] tiles = kingdom.getKingdom();
+
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                final JPanel tile = new TileView(tiles[row][col]);
+                if (row == 0) {
+                    if (col == 0) {
+                        // Top left corner, draw all sides
+                        tile.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+                    }
+                    else {
+                        // Top Edge, draw all sides except left Edge
+                        tile.setBorder(BorderFactory.createMatteBorder(borderWidth,
+                                0,
+                                borderWidth,
+                                borderWidth,
+                                Color.BLACK));
+                    }
+                }
+                else {
+                    if (col == 0) {
+                        // Left-hand Edge, draw all sides except top
+                        tile.setBorder(BorderFactory.createMatteBorder(0,
+                                borderWidth,
+                                borderWidth,
+                                borderWidth,
+                                Color.BLACK));
+                    }
+                    else {
+                        // Neither top Edge nor left Edge, skip both top and left lines
+                        tile.setBorder(BorderFactory.createMatteBorder(0,
+                                0,
+                                borderWidth,
+                                borderWidth,
+                                Color.BLACK));
+                    }
+                }
+                add(tile);
+            }
+        }
+
+    }
+
+    public void updateKingdom(Kingdom kingdom) {
+        // METTRE A JOUR LE ROYAUME
+    }
+
+}
